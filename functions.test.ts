6d64222b731df0775138fb475eda69109090bf37
Macey Bell <maceytbell@gmail.com>
2022-11-04 16:17:07 -0600

    test fix

--- functions.test.ts
+++ functions.test.ts
@@ -1,14 +1,20 @@
 const {shuffleArray} = require('./utils')
 
+const testArr1 = [1, 2, 3, 4]
+const testArr2 = []
+const testArr3=['hi', 'there', 'mom']
+const testArr4=[{'name': 'George'}, {'name': 'Sally'}, {'name': 'Tom'}]
 
 describe('shuffleArray should return', () => {
 
-    test('something', () => {
-    expect(shuffleArray()).toBe(true)
-    } )
+
+
     test('array', () => {
-    expect(Array.isArray(shuffleArray())).toBe(true)
+    expect(Array.isArray(shuffleArray(testArr3))).toBe(true)
     })
-    expect(shuffleArray()).toHaveLength(15)
+
+    test('', () => {
+        expect(shuffleArray(testArr1)).toHaveLength(testArr1.length)
+    })
 
 } )
