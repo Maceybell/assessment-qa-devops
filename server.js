d6d183d91b2afdb4da320afc9e3e21fcb02f87ae
Macey Bell <maceytbell@gmail.com>
2022-11-04 17:08:24 -0600

    I think something just got messed up because I added .env maybe

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const {bots, playerRecord} = require('./data')
 const {shuffleArray} = require('./utils')
-require('dotenv').config()
+
 
 app.use(express.json())
 
